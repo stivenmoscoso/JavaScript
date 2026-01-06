9bb6f9a46daf4b79c225a43feb858d6edd0566b8
stivenmoscoso <stivencolombia@gmail.com>
2026-01-06 08:10:48 -0500

    actualizado

---
+++ alert.js
@@ -0,0 +1,6 @@
+let nombre = prompt("¿Cuál es tu nombre?");
+let lenguaje = prompt("¿Cuál es tu lenguaje de programación favorito?");
+
+
+console.log("Hola " + nombre + ", tu lenguaje de programación favorito es " + lenguaje + ".");
+document.body.innerHTML = "<h1>Hola " + nombre + ", tu lenguaje de programación favorito es " + lenguaje + ".</h1>";
